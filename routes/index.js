9fe77a1be16f1141b142246b7ed2023d8a7e8638
totu <topi.tuulensuu@gmail.com>
2014-12-11 13:50:11 +0100

    aideja

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var fs = require('fs');
+  var audio = [];
+  fs.readdir(__dirname + "/../audio" , function(err, files) {
+    for (var i=0; i<files.length; i++) {
+      audio.push({"file": files[i], "name": files[i].split('.mp3')[0]});
+    }
+    res.render('index', { title: 'Express', files: audio });
+  });
 });
 
+router.get('/test', function(req, res) {
+  res.render('test');
+});
 module.exports = router;
