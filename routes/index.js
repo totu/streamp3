8dfdd9f10b6c883b8063d67307c119300c8c7583
totu <topi.tuulensuu@gmail.com>
2014-12-11 14:34:59 +0100

    added audio folder and test file

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var audio_location = __dirname + "/../audio/";
 
 /* GET home page. */
 router.get('/', function(req, res) {
   var fs = require('fs');
   var audio = [];
-  fs.readdir(__dirname + "/../audio" , function(err, files) {
+  var tmp = null;
+  fs.readdir(audio_location , function(err, files) {
     for (var i=0; i<files.length; i++) {
       audio.push({"file": files[i], "name": files[i].split('.mp3')[0]});
     }
-    res.render('index', { title: 'Express', files: audio });
+    res.render('index', { title: 'Express', results: tmp, files: audio });
   });
 });
 
