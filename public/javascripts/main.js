7dc3204fc2a2a5cdf755cee8f1971f699eaf875b
totu <topi.tuulensuu@gmail.com>
2014-12-12 12:26:46 +0100

    fixed

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,46 +1,8 @@
 $(document).ready(function() {
-
-  // Player
-  filepath = "";
-
-  $('.file').click(function() {
-    filepath = this.getAttribute('data-file');
-    if($('#player').length){
-      $('#player').remove();
-    }
-    $('#player_container').append( "<audio id='player' autoplay='autoplay' controls='controls'>  <source src='play/" + filepath + "' /> </audio> " );
-  });
-
-  // Upload
   $('.file').click(function() {
     if ($(this).data('back'))
       window.location.href = "/";
     else
       window.location.href = $(this).data('file');
-  });
-
-  $('#upload').click(function() {
-    bootbox.dialog({
-      title: "Upload a new audio file",
-      message: "<form id='file_upload_form' method='post' enctype='multipart/form-data'><input type='file' id='file' name='file'></form>",
-      buttons: {
-        cancel: {
-          label: "Cancel",
-          className: "btn-default",
-          callback: function() {
-            $('#file_upload_form').trigger('reset');
-          }
-        },
-        upload: {
-          label: "Upload!",
-          className: "btn-primary",
-          callback: function() {
-            $.post('upload', $('#file_upload_form').serialize(), function(data) {
-              console.log(data);
-            });
-          }
-        }
-      }
-    });
   });
 });
